88e41daa0fd23504dcfbb02d606e2d167b4f9a54
brayanjavi <djbjmo@gmail.com>
2024-11-09 16:33:37 -0600

    incorporacion de PRIMENG en el login y estructura del proyecto en SRC

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service'; 
+import { AuthService } from '../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, ToastModule],
+  providers: [MessageService]
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private messageService: MessageService) { }
 
   async login() {
     try {
       const result = await this.authService.login(this.username, this.password);
       if (result) {
         this.router.navigate(['/home']);
-        
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Login successful' });
       }
     } catch (error) {
       console.error('Login failed', error);
-      alert('Login incorrecto. Por favor, verifica tu nombre de usuario y contraseña.');
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Login failed. Please check your username and password.' });
     }
   }
 }
